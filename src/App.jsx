949187080886dba3796ac0fa1fd6132e14d8a5f8
Saivenkat Mahendrakar <saivenkat.mahendrakar@wissen.com>
2023-04-15 19:07:49 +0530

    Intial Push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Instruction1 from "./components/Instruction1";
+import Instruction from "./components/Instruction/Instruction";
+
+import buttons from "./components/numpad";
+
+function App() {
+  return <Instruction />;
+}
+
+export default App;
