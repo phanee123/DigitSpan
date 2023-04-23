86e22671b35bea8090e5e4e4dfe7c84d74d0d6f8
Saivenkat Mahendrakar <saivenkat.mahendrakar@wissen.com>
2023-04-23 19:13:24 +0530

    Add error boundary

--- src/page/test.jsx
+++ src/page/test.jsx
@@ -6,7 +6,7 @@ import Flash from "../components/flash/Flash";
 const Test = () => {
   const { results } = useContext(ResultsContext);
   const valuesToFlash = results[results.length - 1]?.systemInput;
-  console.log({ valuesToFlash });
+  console.log(valuesToFlash);
   console.log({ results });
   return <Flash toFlash={results[results.length - 1]?.systemInput} />;
 };
