3f8bd922477994870942ace35063709941736d5a
Saivenkat Mahendrakar <saivenkat.mahendrakar@wissen.com>
2023-04-23 17:25:26 +0530

    Remove console logs

--- src/page/test.jsx
+++ src/page/test.jsx
@@ -4,10 +4,9 @@ import { ResultsContext } from "../context/Results";
 import Flash from "../components/flash/Flash";
 
 const Test = () => {
-  const { results, currentStep } = useContext(ResultsContext);
+  const { results } = useContext(ResultsContext);
 
   const valuesToFlash = results[results.length - 1]?.systemInput;
-  console.log(valuesToFlash);
   return <Flash toFlash={results[results.length - 1]?.systemInput} />;
 };
 
